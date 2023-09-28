03d716036b1f8eaede412940681a01bc2290f62d
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-09-28 02:12:28 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Navbar from "./components/navbar/Navbar"
 
 function App() {
  
-
   return (
     <>
       <Navbar />
--- src/components/cardflex/CardFlex.tsx
+++ src/components/cardflex/CardFlex.tsx
@@ -1,5 +1,4 @@
-﻿
-function CardFlex() {
+﻿function CardFlex() {
 
     return (
         <>
